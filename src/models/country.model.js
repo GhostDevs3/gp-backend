5cf9e241b37e06010f4ee62f1859840b49288634
Rafael Fernandez <imraphiki@gmail.com>
2023-02-25 08:54:17 +0100

    [GBE-7] - county model fixed

--- src/models/country.model.js
+++ src/models/country.model.js
@@ -1,15 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const st = Schema.Types;
+const Types = Schema.Types;
 
-const countrySchema = new Schema({
-    name: { type: st.String },
-    ISO_code: { type: st.String },
-    phone_prefix: { type: st.String },
+const schema = new Schema({
+    name: {
+        type: Types.String,
+        trim: true,
+    },
+    ISO_code: {
+        type: Types.String,
+        trim: true,
+    },
+    phone_prefix: {
+        type: Types.String,
+        trim: true,
+    },
     location: {
-        latitude: { type: st.String },
-        longitude: { type: st.String },
+        latitude: {
+            type: Types.String,
+            trim: true,
+        },
+        longitude: {
+            type: Types.String,
+            trim: true,
+        },
     },
 });
 
-module.exports = mongoose.model('country', countrySchema);
+module.exports = mongoose.model('country', schema);
