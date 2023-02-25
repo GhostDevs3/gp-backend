470bb3f06e2839d2953a38ea5cf2e8a06d5ee162
Rafael Fernandez <imraphiki@gmail.com>
2023-02-25 08:45:55 +0100

    [GBE-3] - post model fixed

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Types = Schema.Types;
 
-const postSchema = new Schema({
+const schema = new Schema({
     user_id: {
         type: Types.ObjectId,
         ref: 'users',
@@ -21,4 +21,4 @@ const postSchema = new Schema({
     isPublished: { type: Types.Boolean },
 });
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Post', schema);
