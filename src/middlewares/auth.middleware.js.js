6e468dced416cc45e42d1d751a4a36985d7e9385
Veronika Sanchez Camacho <vsc1972@gmail.com>
2023-03-07 10:06:33 +0100

    [GBE-18]-authMiddleware finish

--- src/middlewares/auth.middleware.js.js
+++ src/middlewares/auth.middleware.js.js
@@ -1,9 +1,10 @@
-const HTTPResponse = require("../utils/http.response");
-const JWTUtils = require("../utils/jwt.utils");
+const HTTPResponse = require('../utils/http.response');
+const JWTUtils = require('../utils/jwt.utils');
+const MongoConnector = require('../utils/mongo.connector');
 
 class AuthMiddleware {
     /**
-     *Method to verify de authorization
+     * Method to verify de authorization
      * @param {import("express").Request} req Request
      * @param {import("express").Response} res Response
      * @param {*} next function to move to the next middleware
@@ -11,27 +12,54 @@ class AuthMiddleware {
     static run(req, res, next) {
         const response = new HTTPResponse(res);
 
-        const authHeaders = req.headers["Authorization"];
+        const authHeaders = req.headers['Authorization'];
         if (!authHeaders) {
-            return response.badRequest("Auth headers Missing", "undefined");
+            return response.badRequest(
+                'Auth headers Missing',
+                'AUTH_HEADERS_MISSING',
+            );
         }
-        //Validate token
-        const tokenValidation = JWTUtils.validateAuthToken(authHeaders);
-        if (tokenValidation.success) {
-            req.payload = tokenValidation.payload;
-            next();
-        } else {
-            if (tokenValidation.reason === JWTUtils.EXPIRED_ERROR) {
-                return response.unauthorized("Expired token");
-            } else if (tokenValidation.reason === JWTUtils.MALFORMED_ERROR) {
-                return response.badRequest("Malformed token", "MALFORMED");
-            } else {
-                return response.error(
-                    "Unknown error",
-                    "UNKNOWN",
-                    tokenValidation.error
+        try {
+            //Validate token
+            const tokenValidation = JWTUtils.validateAuthToken(authHeaders);
+            if (tokenValidation.success) {
+                req.payload = tokenValidation.payload;
+
+                //Check that the token exists in the db with the token value comparing from the one that arrives in the headers
+                const token = MongoConnector.findOne(TokenModel, {
+                    value: authHeaders,
+                });
+                if (!token) {
+                    return response.notFound('User not found');
+                }
+                //Check the user exists with the id that comes in the tokenValidation payload.
+                const user = MongoConnector.findById(
+                    UserModel,
+                    tokenValidation.payload._id,
                 );
+                if (!user) {
+                    return response.notFound('User not found');
+                }
+                req.user = user;
+                next();
+            } else {
+                if (tokenValidation.reason === JWTUtils.EXPIRED_ERROR) {
+                    return response.unauthorized('Expired token');
+                } else if (
+                    tokenValidation.reason === JWTUtils.MALFORMED_ERROR
+                ) {
+                    return response.badRequest('Malformed token', 'MALFORMED');
+                } else {
+                    return response.error(
+                        'Unknown error',
+                        'UNKNOWN',
+                        tokenValidation.error,
+                    );
+                }
             }
+        } catch (error) {
+            console.log(error);
+            return response.error('Unknown error', 'UNKNOWN', error);
         }
     }
 }
